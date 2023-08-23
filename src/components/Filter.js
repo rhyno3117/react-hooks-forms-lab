8d1ed54b067adb00656fea5a373a722d2f2df727
Ryan Meyers <rhynogolfer42@gmail.com>
2023-08-23 16:56:04 -0600

    Adds myfile.md

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,34 @@
+//Determine where you need to add state for this feature. 
+//What components need to know about the search text?
+
+//Once you've determined which component should hold the state for this feature, 
+//set up your initial state, and connect that state to the input field. 
+//Remember, we're trying to make this input a controlled input — 
+//so the input's value should always be in sync with state.
+
+//After you've connected the input to state, 
+//you'll also need to find a way to set state when the input changes. 
+//To get the test passing, you'll need to use a prop called onSearchChange as a callback.
+
+//Finally, after making those changes, 
+//you'll need to use that state value to determine which items are being displayed on the page, 
+//similar to how the category dropdown works.
+
 import React from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({search, onSearchChange, onCategoryChange }) {
+  function handleSearchChange(e) {
+    onSearchChange(e.target.value);
+  }
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
+      <input 
+      type="text" 
+      name="search" 
+      placeholder="Search..." 
+      value={search}
+      onChange={handleSearchChange}
+      />
       <select name="filter" onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,59 @@
-import React from "react";
+//Make all the input fields for this form controlled inputs, 
+//so that you can access all the form data via state. 
+//When setting the initial state for the <select> tag, 
+//use an initial value of "Produce" (since that's the first option in the list).
+
+//Handle the form's submit event, 
+//and use the data that you have saved in state to create a new item object with 
+//the following properties:
+
+//Add the new item to the list by updating state. 
+//To get the test passing, you'll need to use a prop called onItemFormSubmit as a callback 
+//and pass the new item to it.
+
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({ onItemFormSubmit }) {
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("Produce");
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleCategoryChange(event) {
+    setCategory(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    onItemFormSubmit({
+      id: uuid(),
+      name,
+      category,
+    });
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleNameChange}
+        />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select
+          name="category"
+          value={category}
+          onChange={handleCategoryChange}
+        >
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
@@ -21,6 +63,5 @@ function ItemForm(props) {
       <button type="submit">Add to List</button>
     </form>
   );
-}
-
+  }
 export default ItemForm;
